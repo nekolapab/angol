b6d7ff87253c6fbfafb299a97d6fc0d6eaa0930a
nekolapab <nekolapab@gmail.com>
2025-10-15 21:14:12 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,42 +1,98 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
+# To learn more about how to use Nix to configure your environment,
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-23.11"; # Or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.flutter
     pkgs.dart
-    pkgs.jdk21
-    pkgs.unzip
   ];
   # Sets environment variables in the workspace
   env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "Dart-Code.flutter"
-      "Dart-Code.dart-code"
-    ];
-    workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = { };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
-    # Enable previews and customize configuration
+  # Fast way to run services in the workspace.
+  # Use "idx services" command to start/stop/list services.
+  # services = {
+  #   postgres = {
+  #     enable = true;
+  #     package = pkgs.postgresql;
+  #   };
+  # };
+  # Docker, Nix, and Devbox images can be used to set up your environment.
+  # See https://developers.google.com/idx/guides/customize-idx-env#use_a_container_image
+  # containers = {
+  #   project = {
+  #     image = "ubuntu";
+  #     # See https://docs.docker.com/engine/reference/run/ for supported options
+  #     dockerOptions = [ "--volume=/var/run/docker.sock:/var/run/docker.sock" ];
+  #   };
+  # };
+  # IDX extensions to be installed in the workspace.
+  # Use "idx extensions" command to list available extensions.
+  extensions = [
+    "dart-code.dart-code"
+    "dart-code.flutter"
+  ];
+  # Defines a command to be run when the workspace is first created.
+  # onCreate = {
+  #   # Example:
+  #   npm-install = "npm install";
+  #   # Open a file once the workspace is created
+  #   default.openFiles = [ "README.md" ];
+  # };
+  # Defines a command to be run when the workspace is started.
+  onStart = {
+    # This is a harmless command to force an environment rebuild.
+    echo = "echo 'Forcing environment rebuild to fix debugger attachment issue.'";
+  };
+  # Defines a set of tasks to be run in the workspace.
+  # See https://developers.google.com/idx/guides/customize-idx-env#configure_tasks
+  # tasks = {
+  #   # Example:
+  #   npm-run-lint = {
+  #     name = "Lint";
+  #     command = "npm run lint";
+  #   };
+  # };
+
+  # Port forwarding for the workspace.
+  # ports = {
+  #   # Example:
+  #   http = {
+  #     port = 3000;
+  #     onOpen = "open-preview";
+  #   };
+  # };
+
+  # Preview configuration for the workspace.
+  # See https://developers.google.com/idx/guides/customize-idx-env#previews
+  previews = {
+    # The "flutter" manager will start the Flutter web server
+    # and install the Dart and Flutter extensions.
+    enable = true;
     previews = {
-      enable = true;
-      previews = {
-        web = {
-          # Use web-server as the target device for web previews
-          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-          manager = "flutter";
-        };
-        android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
-          manager = "flutter";
-        };
+      web = {
+        # Use web-server as the target device for web previews
+        command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+        manager = "flutter";
+      };
+      android = {
+        command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+        manager = "flutter";
       };
     };
   };
+
+  # Main-branch git integration.
+  # When enabled, IDX will create a new workspace from the main branch,
+  # and push changes to a new branch.
+  # git = {
+  #   main-branch = "main";
+  #   auto-branch = {
+  #     enable = true;
+  #     # The prefix for the new branch name.
+  #     prefix = "idx-";
+  #   };
+  # };
+
 }
